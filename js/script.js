8946af1847f82e5a525c20d76c192d098c8e8dca
Jakub Przydatek <xchubax@wp.pl>
2021-06-14 20:43:42 +0200

    zadanie2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,73 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let playerMove = `papier`;
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+computerMove = 'kamień';}
+else if(randomNumber == 2)
+{computerMove = 'papier';}
+else {(randomNumber == 3)
+{computerMove = 'nożyce';}
+}
+
+printMessage('ruch komputera to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+if(playerInput == '2')
+{playerMove = 'papier';
+}
+if(playerInput == '3')
+{playerMove = 'nożyce';
+}
+
+
+
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('komputer wygrywa!');
+}
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis');
+}
+if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis');
+}
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Komputer wygrywa');
+}
+if( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Komputer Wygrywa');
+}
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+}
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty Wygrywasz');
+}
+if( computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+  printMessage('błąd');
+}
+if( computerMove == 'papier' && playerMove == 'nieznany ruch'){
+  printMessage('błąd');
+}
+if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+  printMessage('błąd');
+}
