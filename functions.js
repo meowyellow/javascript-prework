599fb09e7ce7b40126fafbf837a3780380099b81
Jakub Przydatek <xchubax@wp.pl>
2021-06-14 17:58:13 +0200

    fixed functions

--- functions.js
+++ functions.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
-=======
-function printMessage(''){
+function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
@@ -10,4 +7,3 @@ function printMessage(''){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
->>>>>>> 111e8ef9cc01d0014bc2045b49ebb2ecc84b2a91
