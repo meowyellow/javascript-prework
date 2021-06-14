b5a704c40d0107b35bb3b1eccb8aac9779dec587
Jakub Przydatek <xchubax@wp.pl>
2021-06-14 18:23:17 +0200

    zadanie1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
-​
+
+let playerMove = `papier`;
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
