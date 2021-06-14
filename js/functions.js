111e8ef9cc01d0014bc2045b49ebb2ecc84b2a91
meowyellow <85218994+meowyellow@users.noreply.github.com>
2021-06-14 12:36:43 +0200

    changed text

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 function printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!'){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
-	document.getElementById('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!').appendChild(div);
+	document.getElementById('messages').appendChild(div);
 }
 
 function clearMessages(){
