3f485edd9c7be393139eeafe56a30ae879641176
Jakub Przydatek <xchubax@wp.pl>
2021-06-15 21:54:15 +0200

    zadanie5.5

--- js/script.js
+++ js/script.js
@@ -1,85 +1,85 @@
 
-const getMoveName = function (argMoveId){
+const getMoveName = function (argMoveId) {
 
-    if( argMoveId == 1){
-        return 'kamień';
-    }
-    else if( argMoveId == 2){
-        return 'papier';
-    }
-    else if( argMoveId == 3){
-        return 'nożyce';
-    }
-    else{
-        printMessage('nieznany ruch');
-        return 'nieznany ruch';
-    }
-}     
+  if (argMoveId == 1) {
+    return 'kamień';
+  }
+  else if (argMoveId == 2) {
+    return 'papier';
+  }
+  else if (argMoveId == 3) {
+    return 'nożyce';
+  }
+  else {
+    printMessage('nieznany ruch');
+    return 'nieznany ruch';
+  }
+}
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-    
-  console.log('Gracz wpisał: ' + playerInput);
 
-  let playerMove =  getMoveName(playerInput);
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = getMoveName(playerInput);
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 let computerMove = getMoveName(randomNumber);
 
 
-const displayResult = function(argComputerMove, argPlayerMove){
-    printMessage('Komputer zagrał ' + argComputerMove + ', Ja zagrałem ' + argPlayerMove);
-    
-    if(argPlayerMove == 'Nieznany ruch'){
-        printMessage('Nieznany ruch');
-    }
-    else if(argComputerMove == argPlayerMove){
-        printMessage('Remis');
-    }
-    else if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-        printMessage('Ty Wygrywasz');
-    } 
-    else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
-        printMessage('Przegrywasz')
-    }
-    else if(argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-        printMessage('Ty Wygrywasz')
-    }
-    else if(argComputerMove == 'papier' && argPlayerMove == 'kamień') {
-        printMessage('Przegrywasz!')
-    }
-    else if(argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
-        printMessage('Przegrywasz!')
-    }
-    else if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-        printMessage('Ty Wygrywasz')
-    }
+const displayResult = function (argComputerMove, argPlayerMove) {
+  printMessage('Komputer zagrał ' + argComputerMove + ', Ja zagrałem ' + argPlayerMove);
+
+  if (argPlayerMove == 'Nieznany ruch') {
+    printMessage('Nieznany ruch');
+  }
+  else if (argComputerMove == argPlayerMove) {
+    printMessage('Remis');
+  }
+  else if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+    printMessage('Ty Wygrywasz');
+  }
+  else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+    printMessage('Przegrywasz')
+  }
+  else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+    printMessage('Ty Wygrywasz')
+  }
+  else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
+    printMessage('Przegrywasz!')
+  }
+  else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+    printMessage('Przegrywasz!')
+  }
+  else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+    printMessage('Ty Wygrywasz')
+  }
 }
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 displayResult(computerMove, playerMove);
- 
- /*if (randomNumber == 1) {
-  computerMove = 'kamień';
+
+/*if (randomNumber == 1) {
+ computerMove = 'kamień';
 }
 else if (randomNumber == 2) { computerMove = 'papier'; }
 else {
-  (randomNumber == 3)
-  { computerMove = 'nożyce'; }
+ (randomNumber == 3)
+ { computerMove = 'nożyce'; }
 }
 printMessage('ruch komputera to: ' + computerMove);
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Gracz wpisał: ' + playerInput);
 let playerMove = 'nieznany ruch';
 if (playerInput == '1') {
-  playerMove = 'kamień';
+ playerMove = 'kamień';
 }
 else if (playerInput == '2') {
-  playerMove = 'papier';
+ playerMove = 'papier';
 }
 else if (playerInput == '3') {
-  playerMove = 'nożyce';
+ playerMove = 'nożyce';
 }
 else {
-  printMessage('błąd');
+ printMessage('błąd');
 }
 */
 /*
