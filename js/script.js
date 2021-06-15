3fc0aad209f2ec5aadf2783b066d397b1f2a2b74
Jakub Przydatek <xchubax@wp.pl>
2021-06-15 21:20:32 +0200

    zadanie2.3

--- js/script.js
+++ js/script.js
@@ -1,72 +1,17 @@
-
- function getMoveName(argMoveId) {
-
-  if (argMoveId == 1) {
-    return 'kamień';
-  }
-  else if (argMoveId == 2) {
-    return 'papier';
-  }
-  else if (argMoveId == 3) {
-    return 'nożyce';
-  }
-  else {
-    printMessage('nieznany ruch');
-  }
-}
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = getMoveName(randomNumber);
-
-let playerMove = getMoveName(playerInput);
-
-const displayResult = function (argComputerMove, argPlayerMove) {
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-
-  if (argPlayerMove == 'Nieznany ruch') {
-    printMessage('Nieznany ruch');
-  }
-  else if (argComputerMove == argPlayerMove) {
-    printMessage('Remis');
-  }
-  else if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
-    printMessage('Wygrywasz!');
-  }
-  else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
-    printMessage('Przegrywasz!')
-  }
-  else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-    printMessage('Wygrywasz!')
-  }
-  else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
-    printMessage('Przegrywasz!')
-  }
-  else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
-    printMessage('Przegrywasz!')
-  }
-  else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-    printMessage('Wygrywasz!')
-  }
- displayResult(argcomputerMove, argPlayerMove);
-
-
-
-/*if (randomNumber == 1) {
+if (randomNumber == 1) {
   computerMove = 'kamień';
 }
-else if (randomNumber == 2) { computerMove = 'papier'; }
+else if (randomNumber == 2) 
+{ computerMove = 'papier'; }
 else {
   (randomNumber == 3)
   { computerMove = 'nożyce'; }
 }
-
 printMessage('ruch komputera to: ' + computerMove);
-
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
 console.log('Gracz wpisał: ' + playerInput);
-
 let playerMove = 'nieznany ruch';
 
 if (playerInput == '1') {
@@ -80,12 +25,9 @@ else if (playerInput == '3') {
 }
 else {
   printMessage('błąd');
+ 
 }
-*/
-/*
 printMessage('Twój ruch to: ' + playerMove);
-
-
 if (computerMove == 'kamień' && playerMove == 'papier') {
   printMessage('Ty wygrywasz!');
 }
@@ -107,4 +49,3 @@ else if (computerMove == 'nożyce' && playerMove == 'kamień') {
 else if (computerMove == playerMove) {
   printMessage('Remis');
 }
-*/
